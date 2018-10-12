03db202432bb10ad0b811e36b1f2756fac48f44a
RiotonZong <39319808+riotonchen@users.noreply.github.com>
2018-10-12 13:08:05 +0800

    Update src/views/login/index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -42,14 +42,12 @@
         </el-select>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">{{ $t('login.logIn') }}</el-button>
-      <br>
-      <br>
-      <br>
-      <br>
-      <el-button class="thirdparty-button" type="primary" style="width:86.5%;margin-bottom:30px;" @click="showDialog=true">{{ $t('login.thirdparty') }}</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:15px;" @click.native.prevent="handleLogin">{{ $t('login.logIn') }}</el-button>
+      <!--<el-button class="thirdparty-button" type="primary" style="width:86.5%;margin-bottom:-5px;" @click="showDialog=true">{{ $t('login.thirdparty') }}</el-button>-->
+      <el-button type="primary" style="width:100%;margin-bottom:-200px;margin-left:0px">
+        <router-link to="/home">回首頁</router-link>
+      </el-button>
     </el-form>
-
     <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
       {{ $t('login.thirdpartyTips') }}
       <br>
